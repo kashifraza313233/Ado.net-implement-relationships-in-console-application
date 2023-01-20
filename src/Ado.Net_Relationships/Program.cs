d9ee6e0c314755ceb090c876a21983914b80d5d7
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-20 19:45:19 +0500

    Update Program.cs

--- src/Ado.Net_Relationships/Program.cs
+++ src/Ado.Net_Relationships/Program.cs
@@ -12,25 +12,51 @@ namespace Ado.Net_Relationships
         static void Main(string[] args)
         {
             CrudOperation operation = new CrudOperation();
-            operation.InsertData();
+            operation.GetData();
         }
     }
     public class CrudOperation
     {
         string ConnectionString = @"Data Source=DESKTOP-EOP4ESH\SQLEXPRESS01;Initial Catalog=TeacherCollegeProfile;Integrated Security=True";
-        public void GetData()
+        //public void GetData()
+        //{
+        //    SqlConnection conn = new SqlConnection(ConnectionString);
+        //    string query = "Select * from Teacher";
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand(query, conn);
+        //    SqlDataReader reader = cmd.ExecuteReader();
+        //    while (reader.Read())
+        //    {
+        //        Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString()
+        //            + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
+        //    }
+        //}
+        public void InsertData()
         {
             SqlConnection conn = new SqlConnection(ConnectionString);
-            string query = "Select * from Teacher";
             conn.Open();
+            Console.Write("First_Name:");
+            string First_Name = Console.ReadLine();
+            Console.Write("Last_Name:");
+            string Last_Name = Console.ReadLine();
+            Console.Write("Gender:");
+            string Gender = Console.ReadLine();
+            Console.Write("ContactNo:");
+            string ContactNo = Console.ReadLine();
+            Console.Write("EmailAddress:");
+            string EmailAddress = Console.ReadLine();
+            Console.Write("City:");
+            string City = Console.ReadLine();
+            Console.Write("Address:");
+            string Address = Console.ReadLine();
+            string query = "insert into Teacher values('" + First_Name + "','" + Last_Name + "','" + Gender + "','" + ContactNo + "','" + EmailAddress + "','" + City + "','" + Address + "',)";
             SqlCommand cmd = new SqlCommand(query, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString()
-                    + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
-            }
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Inserted!");
+            conn.Close();
+            Console.ReadKey();
         }
-        
+
     }
+
 }
