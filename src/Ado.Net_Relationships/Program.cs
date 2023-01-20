cef39808a423fb590a66eb3a87ef08012a80ae72
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-20 19:46:46 +0500

    Update Program.cs

--- src/Ado.Net_Relationships/Program.cs
+++ src/Ado.Net_Relationships/Program.cs
@@ -18,19 +18,19 @@ namespace Ado.Net_Relationships
     public class CrudOperation
     {
         string ConnectionString = @"Data Source=DESKTOP-EOP4ESH\SQLEXPRESS01;Initial Catalog=TeacherCollegeProfile;Integrated Security=True";
-        //public void GetData()
-        //{
-        //    SqlConnection conn = new SqlConnection(ConnectionString);
-        //    string query = "Select * from Teacher";
-        //    conn.Open();
-        //    SqlCommand cmd = new SqlCommand(query, conn);
-        //    SqlDataReader reader = cmd.ExecuteReader();
-        //    while (reader.Read())
-        //    {
-        //        Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString()
-        //            + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
-        //    }
-        //}
+        public void GetData()
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            string query = "Select * from Teacher";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString()
+                    + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
+            }
+        }
         public void InsertData()
         {
             SqlConnection conn = new SqlConnection(ConnectionString);
