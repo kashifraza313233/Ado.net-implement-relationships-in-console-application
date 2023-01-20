fe5114d12da96b9fa4c770a4c142a5ee129fd364
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-20 19:36:47 +0500

    Update Program.cs

--- src/Ado.Net_Relationships/Program.cs
+++ src/Ado.Net_Relationships/Program.cs
@@ -20,7 +20,6 @@ namespace Ado.Net_Relationships
         string ConnectionString = @"Data Source=DESKTOP-EOP4ESH\SQLEXPRESS01;Initial Catalog=TeacherCollegeProfile;Integrated Security=True";
         public void GetData()
         {
-
             SqlConnection conn = new SqlConnection(ConnectionString);
             string query = "Select * from Teacher";
             conn.Open();
@@ -32,30 +31,6 @@ namespace Ado.Net_Relationships
                     + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
             }
         }
-        public void InsertData()
-        {
-            SqlConnection conn = new SqlConnection(ConnectionString);
-            conn.Open();
-            Console.Write("First_Name:");
-            string First_Name = Console.ReadLine();
-            Console.Write("Last_Name:");
-            string Last_Name = Console.ReadLine();
-            Console.Write("Gender:");
-            string Gender = Console.ReadLine();
-            Console.Write("ContactNo:");
-            string ContactNo = Console.ReadLine();
-            Console.Write("EmailAddress:");
-            string EmailAddress = Console.ReadLine();
-            Console.Write("City:");
-            string City = Console.ReadLine();
-            Console.Write("Address:");
-            string Address = Console.ReadLine();
-            string query = "insert into Teacher(First_Name,Last_Name,Gender,ContactNo,EmailAddress,City,Address)values('" + First_Name + "','" + Last_Name + "','" + Gender + "','" + ContactNo + "','" + EmailAddress + "','" + City + "','" + Address + "',)";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Data Inserted!");
-            conn.Close();
-            Console.ReadKey();
-        }
+        
     }
 }
