aff28555781f18c6d7e04b317ed0c237897f3f95
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-20 23:06:10 +0500

    Update Program.cs

--- src/Ado.Net_Relationships/Program.cs
+++ src/Ado.Net_Relationships/Program.cs
@@ -11,8 +11,29 @@ namespace Ado.Net_Relationships
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\t\t\t\t\t\t\t\tTeacher College Profile");
+            Console.WriteLine("\n\n");
+            Console.WriteLine(" Enter 1 to Insert Data:\t");
+            Console.WriteLine(" Enter 2 to Get Data:\t");
+            Console.WriteLine(" Enter 3 to Delete Data:\t");
+            Console.WriteLine(" Enter 4 to Update Data:\t");
+            Console.WriteLine(" Enter 0 to Exist:\t");
+           
             CrudOperation operation = new CrudOperation();
-            operation.InsertData();
+            int n;
+            Console.WriteLine("Enter Number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n");
+            switch (n) 
+            {
+                case 1:
+                    operation.InsertData();
+                    break;
+                case 2:
+                    operation.GetData();
+                    break;
+                   
+            }
         }
     }
     public class CrudOperation
@@ -20,41 +41,57 @@ namespace Ado.Net_Relationships
         string ConnectionString = @"Data Source=DESKTOP-EOP4ESH\SQLEXPRESS01;Initial Catalog=TeacherCollegeProfile;Integrated Security=True";
         public void GetData()
         {
-            SqlConnection conn = new SqlConnection(ConnectionString);
-            string query = "Select * from Teacher";
-            conn.Open();
-            SqlCommand cmd = new SqlCommand(query, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+           try
             {
-                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString()
-                    + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                string query = "Select * from Teacher";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString()
+                        + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString());
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);   
             }
         }
         public void InsertData()
         {
-            SqlConnection conn = new SqlConnection(ConnectionString);
-            conn.Open();
-            Console.Write("First_Name:");
-            string First_Name = Console.ReadLine();
-            Console.Write("Last_Name:");
-            string Last_Name = Console.ReadLine();
-            Console.Write("Gender:");
-            string Gender = Console.ReadLine();
-            Console.Write("ContactNo:");
-            string ContactNo = Console.ReadLine();
-            Console.Write("EmailAddress:");
-            string EmailAddress = Console.ReadLine();
-            Console.Write("City:");
-            string City = Console.ReadLine();
-            Console.Write("Address:");
-            string Address = Console.ReadLine();
-            string query = "insert into Teacher values('" + First_Name + "','" + Last_Name + "','" + Gender + "','" + ContactNo + "','" + EmailAddress + "','" + City + "','" + Address + "',)";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Data Inserted!");
-            conn.Close();
-            Console.ReadKey();
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                conn.Open();
+                Console.WriteLine("Please Enter Data:\n");
+                Console.Write("First_Name:");
+                string First_Name = Console.ReadLine();
+                Console.Write("Last_Name:");
+                string Last_Name = Console.ReadLine();
+                Console.Write("Gender:");
+                string Gender = Console.ReadLine();
+                Console.Write("ContactNo:");
+                string ContactNo = Console.ReadLine();
+                Console.Write("EmailAddress:");
+                string EmailAddress = Console.ReadLine();
+                Console.Write("City:");
+                string City = Console.ReadLine();
+                Console.Write("Address:");
+                string Address = Console.ReadLine();
+                string query = "insert into Teacher values('" + First_Name + "','" + Last_Name + "','" + Gender + "','" + ContactNo + "','" + EmailAddress + "','" + City + "','" + Address + "')";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Data Successfully Inserted!");
+                conn.Close();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+            
         }
 
     }
